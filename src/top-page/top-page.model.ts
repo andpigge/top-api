271cf415e6d7cc0c4cb2b2cc3876c6ac5fd74627
Rustam <Rustamaaaa@bk.ru>
2022-05-14 16:28:00 +0300

    feat: реализовать модель страницы с курсами

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,28 @@
+/* Вся страница */
+
+export enum TopLevelCategory {
+	Courses,
+	Services,
+	Books,
+	Products
+}
+
+export class TopPageModel {
+	firstCategory: TopLevelCategory;
+	secondCategory: string;
+	title: string;
+	category: string;
+	headHunter?: {
+		count: number;
+		juniorSalary: number;
+		middleSalary: number;
+		seniorSalary: number;
+	};
+	advantages: {
+		title: string;
+		description: string;
+	}[];
+	seoText: string;
+	tagsTitle: string;
+	tags: string[];
+}
