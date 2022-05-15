7623898c9114565793148427492ede3075456b5d
Rustam <Rustamaaaa@bk.ru>
2022-05-15 15:24:36 +0300

    feat: подключить модуль для работы с локальным окружением

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot(),
 		AuthModule,
 		TopPageModule,
 		ProductModule,
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -8,16 +8,18 @@ import {
 	Patch,
 	Post 
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 /* CRUD */
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService) {}
 
 	@Post('create')
 	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
-
+		this.configService.get('TEST');
 	}
 
 	@Get(':id')
